75eabae04a75d7748c52a4b0afe44343c1181180
Barboza-Dev7 <raul.barboza.dev@gmail.com>
2026-04-29 19:11:43 -0500

    Correccion prueba

--- prueba.go
+++ prueba.go
@@ -4,31 +4,28 @@ import (
 	"log"
 	"net"
 	"io"
-	"encoding/hex"
 )
 
 func handleConnection(conn net.Conn){
-	defer conn.Close()
 	log.Println("Conectado: ", conn.RemoteAddr())
 
 	buf := make([]byte, 1024)
-	for {
-		n, err := conn.Read(buf)
-		if err == io.EOF {
-			log.Println("Moto desconectada")
-			return
-		}
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		raw := buf[:n]
-		log.Println("Recibido: ", hex.EncodeToString(raw))
+	data, err := conn.Read(buf)
+	raw := buf(:data)
+
+	if err == io.EOF {
+		log.Println("Desconectado: ", conn.RemoteAddr())
+		return
 	}
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(data)
 }
 
 func main(){
-	listener, err := net.Listen("tcp", ":5000")
+	listen, err := net.Listen("tcp", ":5000")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,7 +33,7 @@ func main(){
 	log.Println("Escuchando...")
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := listen.Accept()
 		if err != nil {
 			log.Println(err)
 			continue
